7e721a111061bb0d2635b380091c9f86ceb6cf2b
Tommy Creuzer <TommyCreuzer@protonmail.com>
2025-02-11 00:15:48 -0600

    c++ file actually added

---
+++ indifference.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main(){
+string good1, good2;
+double cost1, cost2;
+cout << "To calculate indifference, although the model we will be using is not ideal (It does not match with marginal utility due to requiring much more user and programmer input), it will give an idea for indifference." << endl << "Remember indifference is the combination of two goods that give equal utility." << endl;
+
+}
